cb0468a7b2e87b01dc778a365e49e22cb64c51de
ABD\Abdalla <abdalla0abu0amsha@gmail.com>
2019-03-20 04:46:32 +0200

    Create Language Model and Language Date service.

---
+++ Warehouses.UI/Data/ILanguagesDataService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Warehouse.Model;
+
+namespace Warehouses.UI.Data
+{
+    public interface ILanguagesDataService
+    {
+        IEnumerable<Language> GetAll();
+    }
+}
---
+++ Warehouses.UI/Data/LanguagesDataService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Warehouse.Model;
+
+namespace Warehouses.UI.Data
+{
+    public class LanguagesDataService : ILanguagesDataService
+    {
+        public IEnumerable<Language> GetAll()
+        {
+            yield return new Language { Name = "Language 1" };
+            yield return new Language { Name = "Language 2" };
+            yield return new Language { Name = "Language 3" };
+            yield return new Language { Name = "Language 4" };
+            yield return new Language { Name = "Language 5" };
+        }
+    }
+}
